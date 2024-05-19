3f86c2d800f6d709ceb506c8a3278a263bc2d909
ckesurf <ckesurf@gmail.com>
2024-05-19 13:12:24 -0700

    add all files to public folder

---
+++ public/app.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="bg-info">
+            helloo footer chris
+            <i className="fab fa-linkedin-in"></i>
+            <i className="fab fa-github"></i>
+        </footer>
+    );
+}
+
+ReactDOM.render(
+    <Footer />,
+    document.getElementById('root-footer')
+);
