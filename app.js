e05f256386a6d03ebd3ce376440b98bb1448c926
ckesurf <ckesurf@gmail.com>
2024-05-31 17:44:38 -0700

    footer fixup

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const Footer = () => {
     return (
-        <footer className="bg-info">
-            helloo footer chris
-            <i className="fab fa-linkedin-in"></i>
-            <i className="fab fa-github"></i>
+        <footer className="bg-info template-demo">
+
+            <a href="https://www.linkedin.com/in/chris-erlendson-3134aa27/" target="_blank" className="btn btn-social-icon btn-linkedin btn-rounded footer-btn">
+                <i class="fa fa-linkedin">
+
+                </i>
+            </a>
+
+            <a href="https://www.linkedin.com/in/chris-erlendson-3134aa27/" target="_blank" className="btn btn-social-icon btn-github btn-rounded footer-btn">
+                <i className="fa fa-github"></i>
+            </a>
+
         </footer>
     );
 }
