4e786ad0ceb380ad7bd3560039ec2afcd3b62922
ckesurf <ckesurf@gmail.com>
2024-06-03 13:21:57 -0700

    comment out duplicate footer

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const Footer = () => {
     return (
-        <footer className="bg-info template-demo">
+        <></>
+        // <footer className="bg-info template-demo">
 
-            <a href="https://www.linkedin.com/in/chris-erlendson-3134aa27/" target="_blank" className="btn btn-social-icon btn-linkedin btn-rounded footer-btn">
-                <i class="fa fa-linkedin">
+        //     <a href="https://www.linkedin.com/in/chris-erlendson-3134aa27/" target="_blank" className="btn btn-social-icon btn-linkedin btn-rounded footer-btn">
+        //         <i class="fa fa-linkedin">
 
-                </i>
-            </a>
+        //         </i>
+        //     </a>
 
-            <a href="https://www.linkedin.com/in/chris-erlendson-3134aa27/" target="_blank" className="btn btn-social-icon btn-github btn-rounded footer-btn">
-                <i className="fa fa-github"></i>
-            </a>
+        //     <a href="https://www.linkedin.com/in/chris-erlendson-3134aa27/" target="_blank" className="btn btn-social-icon btn-github btn-rounded footer-btn">
+        //         <i className="fa fa-github"></i>
+        //     </a>
 
-        </footer>
+        // </footer>
     );
 }
 
